59c19c6323d65ff6bcc2ae10ac67a5877d616719
Rabindra Joshi <therj@users.noreply.github.com>
2018-02-13 16:01:20 +0545

    Real time feedback

--- index.js
+++ index.js
@@ -16,7 +16,12 @@ io.on("connection", function(socket){
     socket.on("chat", function (data) {
         console.log(data);
         io.sockets.emit("chat", data);
-    } );
+    });
+
+    socket.on("typing", function (data) {
+        console.log(data);
+        socket.broadcast.emit("typing", data)
+    });
 });
 
 
--- public/chat.js
+++ public/chat.js
@@ -6,6 +6,7 @@ var message = document.querySelector("#message");
 var handle = document.querySelector("#handle");
 var btn = document.querySelector("#send");
 var output  = document.querySelector("#output");
+var feedback  = document.querySelector("#feedback");
 
 
 //Emit event
@@ -16,8 +17,15 @@ btn.addEventListener("click", function () {
     });
 });
 
-//Listen event
+message.addEventListener("keypress", function () {
+    socket.emit("typing", handle.value);
+});
 
+//Listen event
 socket.on("chat", function (data) {
     output.innerHTML += "<p><strong>" + data.handle +":</strong>" + data.message + "</p>";
+});
+
+socket.on("typing", function (data) {
+    feedback.innerHTML = "<p> <em>" + data + " is typing a message... </em> </p>";
 });
